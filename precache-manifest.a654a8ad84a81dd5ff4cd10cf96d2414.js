922113818bd637906b84ff026abb16eac9e9f2c3
Adham Sewelam <adham.h.sewelam@gmail.com>
2023-02-28 02:47:05 +0200

    Updates

---
+++ precache-manifest.a654a8ad84a81dd5ff4cd10cf96d2414.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "31c6663c1e6466e28834",
+    "url": "/Adhams-Portfolio/static/css/main.6aeeea17.chunk.css"
+  },
+  {
+    "revision": "31c6663c1e6466e28834",
+    "url": "/Adhams-Portfolio/static/js/main.31c6663c.chunk.js"
+  },
+  {
+    "revision": "a1f65421c17deb45794f",
+    "url": "/Adhams-Portfolio/static/css/1.08f785e4.chunk.css"
+  },
+  {
+    "revision": "a1f65421c17deb45794f",
+    "url": "/Adhams-Portfolio/static/js/1.a1f65421.chunk.js"
+  },
+  {
+    "revision": "05f6a5f7bbd77909e6c5",
+    "url": "/Adhams-Portfolio/static/js/runtime~main.05f6a5f7.js"
+  },
+  {
+    "revision": "231fc2d544e04cc7416f9cbbf8fad32d",
+    "url": "/Adhams-Portfolio/static/media/About.231fc2d5.jpg"
+  },
+  {
+    "revision": "40a8799fc5074628b68fc1ad02d94839",
+    "url": "/Adhams-Portfolio/static/media/Adham Sewelam CV.40a8799f.pdf"
+  },
+  {
+    "revision": "abbba27764f452676ea9293e9cbd3690",
+    "url": "/Adhams-Portfolio/static/media/testimonial1.abbba277.png"
+  },
+  {
+    "revision": "f322c9f87a1abdf9fea676a2154f12fe",
+    "url": "/Adhams-Portfolio/static/media/testimonial2.f322c9f8.png"
+  },
+  {
+    "revision": "2b13fc93eccfd5381a8302678af3325b",
+    "url": "/Adhams-Portfolio/static/media/testimonial3.2b13fc93.png"
+  },
+  {
+    "revision": "231fc2d544e04cc7416f9cbbf8fad32d",
+    "url": "/Adhams-Portfolio/static/media/Profile.231fc2d5.jpg"
+  },
+  {
+    "revision": "a477bf5429228423af9270b381162a0c",
+    "url": "/Adhams-Portfolio/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Adhams-Portfolio/precache-manifest.a654a8ad84a81dd5ff4cd10cf96d2414.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Adhams-Portfolio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
